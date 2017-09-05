020f8e24257179ab43d0738b92af61053b415e45
shanyangyang <shanyangyang@163.com>
2017-09-05 21:05:01 +0800

    添加和编辑公用模态框

--- public/js/course-lesson.js
+++ public/js/course-lesson.js
@@ -1,7 +1,7 @@
 define(['jquery','template','util','bootstrap','form'],function($,template,util){
 	//设置导航菜单选中高亮显示
 	util.setMenu('/course/list');
-	//获取课程菜单
+	//获取课程id
 	var csId=util.qs('cs_id');
 	//通过后台接口获取课时信息
 	$.ajax({
@@ -12,8 +12,35 @@ define(['jquery','template','util','bootstrap','form'],function($,template,util)
 		},
 		dataType:'json',
 		success:function(data){
+			//渲染页面
 			var html=template('courseTpl',data.result);
 			$('#courseInfo').html(html);
+			//实现添加课时功能
+			$('#addBtn').click(function(){
+				$('#chapterModal').modal();//modal()方法 弹出模态框
+				var html=template('modalTpl',{operate:'添加课时'});
+				$('#modalInfo').html(html);
+			});
+			//实现编辑课时功能
+			$('.editLesson').click(function(){
+				var ctId=$(this).attr('data-ctId');
+				$.ajax({
+					type:'get',
+					url:'/api/course/chapter/edit',
+					data: {
+						ct_id: ctId
+					},
+					dataType: 'json',
+					success:function(data){
+						console.log(data);
+							data.result.operate='编辑课时';
+							var html=template('modalTpl',data.result);
+							$('#modalInfo').html(html);							
+					}
+
+				});
+				$('#chapterModal').modal();
+			});
 		}
 	});
 
